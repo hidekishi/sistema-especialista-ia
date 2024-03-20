58f12f295d244a4a5c5cec9cba1f00548bdfc7b1
gabriel-r-s <author@email>
2024-03-20 13:58:59 -0300

    documentar main.py

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 from random import choice
 from pyke import knowledge_engine, krb_traceback
 
+# inicializacao da knowlege engine
 engine = knowledge_engine.engine(__file__)
 
 
-def bc_test():
+# executar backwards-chaining
+def bc():
+    # para permitir que possa executar mais que uma vez
     engine.reset()
+    # obtendo regras do arquivo "rules.krb"
     engine.activate("rules")
 
     try:
         with engine.prove_goal("rules.what_to_play($game)") as gen:
+            # tentamos obter somente a primeira sugestão
             vars_, _ = next(gen, (None, None))
             if vars_ is not None:
                 game = vars_["game"]
-                print(f"Você deveria jogar: {vars_['game']}")
+                print(f"Você deveria jogar: {game}")
+            # se não existe, então não for possível sugerir um jogo
+            # exibe uma mensagem para informar
             else:
                 joga_nada = [
-                    "Ah, não joga nada então",
+                    "Não joga nada então",
                     "Então vai ler um livro",
                     "Vai dar uma volta na quadra",
                 ]
                 print(choice(joga_nada))
     except Exception as e:
+        # exibir traceback do pyke em caso de erro
         krb_traceback.print_exc()
         raise e
 
 
 def main():
     print("EXPERT SYSTEM")
-    bc_test()
+    bc()
 
 
 if __name__ == "__main__":
